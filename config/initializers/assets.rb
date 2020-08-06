f5917f338ed8400b059d5fb618a958b7a1f153ef
Daniel <dgl.daniel@outlook.com.br>
2020-08-05 23:08:39 -0300

    Porting the Gentelella theme to the User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,7 +14,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # /app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css
+  users_backoffice.js users_backoffice.css )
 
 # /lib/assets
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
+  custom.js custom.css
+  img.jpg )
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery2.js )

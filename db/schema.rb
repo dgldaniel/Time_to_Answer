1492bd94d0302ec1cecfb0ddbc2096286e4efb4f
Daniel <dgl.daniel@outlook.com.br>
2020-08-08 14:06:30 -0300

    Create question task

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_013812) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "description"
+    t.text "description", null: false
     t.integer "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_013812) do
   end
 
   create_table "subjects", force: :cascade do |t|
-    t.string "description"
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

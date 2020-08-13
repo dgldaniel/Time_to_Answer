2029c05dba53e3077920569b24182132cacb261f
Daniel <dgl.daniel@outlook.com.br>
2020-08-12 21:37:02 -0300

    Enabling question answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   namespace :site do
-    get 'welcome/index'
-    get 'search', to: 'search#questions'
+    get  'welcome/index'
+    get  'search', to: 'search#questions'
+    post 'answer', to: 'answer#question'
   end
   namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins # Administradores
+    resources :admins   # Administradores
     resources :subjects # Assuntos/Áreas
     resources :questions # Perguntas
   end
